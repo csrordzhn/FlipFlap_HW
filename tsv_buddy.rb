55041b160693d99fcaeca14bcdbaeeec28ba3df3
Cesar Ordonez <Cesar@Cesars-MacBook-Pro.local>
2015-10-02 14:49:02 +0800

    2015-10-02: First draft of homework

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,12 +7,24 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    data_arr=[]
+    tsv_arr = tsv.lines
+    header_arr = tsv_arr[0].chomp("\n").split("\t")
 
+    (1..tsv_arr.size-1).each do |i|
+      row_info = tsv_arr[i].chomp("\n").split("\t")
+      data_arr << (header_arr.zip(row_info)).to_h
+    end
+    @data = data_arr
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
 
+    tsv = @data[0].keys.join("\t") + "\n"
+    yaml_data.each do |hash|
+      tsv += hash.values.join("\t") + "\n"
   end
+  tsv
 end
