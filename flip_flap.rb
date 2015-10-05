21902f3510323c131ed03eafe08ecbba4157d1f8
soa <soa@localhost.localdomain>
2015-10-05 18:27:15 +0800

    Fixed Rubocop warnings

--- flip_flap.rb
+++ flip_flap.rb
@@ -5,14 +5,14 @@ class FlipFlap
   # Do NOT create an initialize method
   include TsvBuddy
 
+  # this method takes a Yaml string and create a data structure to put into `@data`.
+  # You should be able to do this with one line of code using the `yaml` library's load method.
   def take_yaml(yml)
-     #this method takes a Yaml string and create a data structure to put into `@data`.
-     #You should be able to do this with one line of code using the `yaml` library's load method.
-     @data = YAML.load(yml)
-  end
-  def to_yaml
-    #: this method should return `@data` in Yaml format.
-    @data.to_yaml
+    @data = YAML.load(yml)
   end
 
+  #: this method should return `@data` in Yaml format.
+  def to_yaml
+    @data.to_yaml
+  end
 end
