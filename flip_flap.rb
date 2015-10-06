7afe18de9250ff00ff6ec61743c8cecbb1d05d76
soa <soa@localhost.localdomain>
2015-10-06 23:48:53 +0800

    Included YAMLBuddy module with YAML conversion functionalities

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,18 +1,9 @@
 require_relative 'tsv_buddy'
+require_relative 'yaml_buddy'
 require 'yaml'
-
+# class FlipFLap
 class FlipFlap
   # Do NOT create an initialize method
   include TsvBuddy
-
-  # this method takes a Yaml string and create a data structure to put into `@data`.
-  # You should be able to do this with one line of code using the `yaml` library's load method.
-  def take_yaml(yml)
-    @data = YAML.load(yml)
-  end
-
-  #: this method should return `@data` in Yaml format.
-  def to_yaml
-    @data.to_yaml
-  end
+  include YamlBuddy
 end
