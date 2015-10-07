c11845c6b6eb4bc252c5fb2bd70a66898f6b8516
Calvin Jeng <locky4567@gmail.com>
2015-10-07 17:39:04 +0000

    smallfix in yml.rb and revise flip_flap, test passed

--- flip_flap.rb
+++ flip_flap.rb
@@ -5,16 +5,4 @@ class FlipFlap
   # Do NOT create an initialize method
   include TsvBuddy
   include YmlBuddy
-
-  # this method takes a Yaml string and create a data structure to put into `@data`.
-  # You should be able to do this with one line of code using the `yaml` library's load method.
-  def take_yaml(yml)
-    @data = YAML.load(yml)
-  end
-
-  #: this method should return `@data` in Yaml format.
-  def to_yaml
-    @data.to_yaml
-  end
-
 end
--- yml_buddy.rb
+++ yml_buddy.rb
@@ -1,9 +1,8 @@
 module YmlBuddy
-    
+  require 'yaml'  
+  
   attr_accessor :data
 
-  require 'yaml'
-      
   def take_yaml(yml)
      #this method takes a Yaml string and create a data structure to put into `@data`.
      #You should be able to do this with one line of code using the `yaml` library's load method.
